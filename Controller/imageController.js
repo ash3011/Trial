ecd0eae1b0fd45eed587988900b981f08603461e
Aishwarya Kamlakar Sawant <91437970+ash3011@users.noreply.github.com>
2022-03-27 16:01:58 +0100

    Update imageController.js

--- Controller/imageController.js
+++ Controller/imageController.js
@@ -13,7 +13,6 @@ const fs = require('fs')
 
 
 //Creating a new instance of S3:
-const AWS = require("aws-sdk");
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
