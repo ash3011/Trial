ba161eff70d8abde7ce5783acd1c401340e2ffd6
Aishwarya Kamlakar Sawant <91437970+ash3011@users.noreply.github.com>
2022-04-05 05:49:09 -0400

    Update provisioners.sh

--- packer/provisioners.sh
+++ packer/provisioners.sh
@@ -4,6 +4,8 @@ sleep 30
 
 sudo yum update -y
 sleep 10
+sudo yum install ruby wget unzip -y
+sleep 10
 sudo yum install git make gcc -y
 sleep 10
 sudo amazon-linux-extras install epel
@@ -17,5 +19,24 @@ sudo yum install -y nodejs
 sleep 10
 sudo npm install -g pm2
 sleep 10
-mkdir /home/ec2-user/node-app
-chown ec2-user:ec2-user /home/ec2-user/node-app
+
+# install codedeploy agent
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent status
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
+
+ls
+cd /tmp/
+echo "$(pwd)"
+ls
+cp webservice.zip /home/ec2-user/
+cd /home/ec2-user/
+unzip -q webservice.zip
+ls -ltr
+chown ec2-user:ec2-user /home/ec2-user/webservice
+cd webservice 
+ls -ltr
